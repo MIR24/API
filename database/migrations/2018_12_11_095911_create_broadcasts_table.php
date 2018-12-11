b08229073175f3349642a7454defb3fd194d6cc2
IvanZ <apsent86@gmail.com>
2018-12-11 14:03:06 +0300

    add Chanel Broudcasts migrations

---
+++ database/migrations/2018_12_11_095911_create_broadcasts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBroadcastsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('broadcasts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('title');
+            $table->text('subtitle');
+            $table->integer('age');
+            $table->text('DayOfWeek');
+            $table->dateTime('begin');
+            $table->dateTime('end');
+            $table->integer('category_id')->unsigned()->nullable();
+            $table->foreign('category_id')->references('id')->on('categories');
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('broadcasts');
+    }
+}
---
+++ database/migrations/2018_12_11_103339_create_channels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChannelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('channel', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('name');
+            $table->text('stream_shift');
+            $table->text('stream_live');
+            $table->text('logo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('channel');
+    }
+}
