e80706305ee3200ec2e4c037a11fd935a100cf34
IvanZ <apsent86@gmail.com>
2018-12-12 10:48:54 +0300

    add timer replacer

--- app/Http/Controllers/ArchiveController.php
+++ app/Http/Controllers/ArchiveController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Archive;
+use App\Library\Services\TimeReplacer\TimeReplacer;
 use Illuminate\Http\Request;
 
 class ArchiveController extends Controller
 {
-    public function show()
+    public function show(TimeReplacer $replacer)
     {
-        return response()->json(Archive::GetForApi()->get());
+        return response()->json( $replacer->replace(Archive::GetForApi()->get()));
     }
 }
