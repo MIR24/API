ec48af5d5aedfea6dd1822062746ea73bc5fb19a
IvanZ <apsent86@gmail.com>
2018-12-27 14:32:46 +0300

    add middleware api  token

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('smart/v1/categories','CategoryController@show');
+Route::get('smart/v1/categories','CategoryController@show')->middleware('token');
 
 Route::get('smart/v1/channels','ChannelsController@show');
 
