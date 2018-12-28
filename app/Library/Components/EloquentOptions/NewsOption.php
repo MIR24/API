628ba5714bf213714307ac9e468c63f9200fa91f
Обловацкий Юрий <jury@observat.by>
2018-12-28 11:41:40 +0300

    New file: app/Library/Components/EloquentOptions/NewsOption.php

---
+++ app/Library/Components/EloquentOptions/NewsOption.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Library\Components\EloquentOptions;
+
+
+class NewsOption
+{
+    private $page = 1;
+    private $limit = 20;
+    private $category = null;
+    private $newsID = null;
+    private $actual = false;
+    private $onlyVideo = false;
+    private $onlyWithGallery = false;
+    private $lastNews = false;
+    private $tags = null;
+    private $countryID = null;
+    private $preSearch = null;
+    private $ignoreId;
+//    private $buffer = null;
+
+//    public Boolean checkBufferForID(Integer id) {
+//        Boolean exists = false;
+//        if (buffer.contains(id)) {
+//            exists = Boolean.TRUE;
+//        }
+//        return exists;
+//    }
+//
+//    public void saveToBuffer(Integer id) {
+//        buffer.add(id);
+//    }
+
+    public function getPage(): int
+    {
+        return $this->page;
+    }
+
+    public function setPage(int $page): NewsOption
+    {
+        $this->page = $page;
+        return $this;
+    }
+
+    public function getLimit(): int
+    {
+        return $this->limit;
+    }
+
+    public function setLimit(int $limit): NewsOption
+    {
+        $this->limit = $limit;
+        return $this;
+    }
+
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    public function setCategory($category)
+    {
+        $this->category = $category;
+        return $this;
+    }
+
+    public function getNewsID()
+    {
+        return $this->newsID;
+    }
+
+    public function setNewsID($newsID)
+    {
+        $this->newsID = $newsID;
+        return $this;
+    }
+
+    public function isActual(): bool
+    {
+        return $this->actual;
+    }
+
+    public function setActual(bool $actual): NewsOption
+    {
+        $this->actual = $actual;
+        return $this;
+    }
+
+    public function isOnlyVideo(): bool
+    {
+        return $this->onlyVideo;
+    }
+
+    public function setOnlyVideo(bool $onlyVideo): NewsOption
+    {
+        $this->onlyVideo = $onlyVideo;
+        return $this;
+    }
+
+    public function isOnlyWithGallery(): bool
+    {
+        return $this->onlyWithGallery;
+    }
+
+    public function setOnlyWithGallery(bool $onlyWithGallery): NewsOption
+    {
+        $this->onlyWithGallery = $onlyWithGallery;
+        return $this;
+    }
+
+    public function isLastNews(): bool
+    {
+        return $this->lastNews;
+    }
+
+    public function setLastNews(bool $lastNews): NewsOption
+    {
+        $this->lastNews = $lastNews;
+        return $this;
+    }
+
+    public function getTags()
+    {
+        return $this->tags;
+    }
+
+    public function setTags($tags)
+    {
+        $this->tags = $tags;
+        return $this;
+    }
+
+    public function getCountryID()
+    {
+        return $this->countryID;
+    }
+
+    public function setCountryID($countryID)
+    {
+        $this->countryID = $countryID;
+        return $this;
+    }
+
+    public function getPreSearch()
+    {
+        return $this->preSearch;
+    }
+
+    public function setPreSearch($preSearch)
+    {
+        $this->preSearch = $preSearch;
+        return $this;
+    }
+
+    public function getIgnoreId()
+    {
+        return $this->ignoreId;
+    }
+
+    public function setIgnoreId($ignoreId)
+    {
+        $this->ignoreId = $ignoreId;
+        return $this;
+    }
+}
