c3b9dac6a17c8d77f3b34c6d0468434eb9cc82a9
Обловацкий Юрий <jury@observat.by>
2019-01-23 09:33:12 +0300

    New file: app/Http/Resources/NewsItemResource.php

---
+++ app/Http/Resources/NewsItemResource.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Resources;
+
+
+use App\Http\AppJsonResource;
+
+
+class NewsItemResource extends AppJsonResource
+{
+    public function toArray($request)
+    {
+        $result = [
+            "id" => $this->id,
+            "date" => $this->date,
+            "shortText" => $this->shortText,
+            "shortTextSrc" => $this->shortTextSrc,
+            "title" => $this->title,
+            "imageID" => $this->imageID,
+            "categoryID" => $this->categoryID,
+            "serieID" => $this->serieID,
+            "videoID" => $this->videoID,
+            "episodeID" => $this->episodeID,
+            "copyright" => $this->copyright,
+            "copyrightSrc" => $this->copyrightSrc,
+            "rushHourNews" => boolval($this->rushHourNews),
+            "topListNews" => boolval($this->topListNews),
+            "hasGallery" => boolval($this->hasGallery),
+            "videoDuration" => $this->videoDuration,
+            "tags" => $this->tags,
+            "country" => $this->country,
+            "newsText" => $this->newsText
+//            "newsText" => [
+//                "textWithTags" => $this->newsText->textWithTags,
+//                "textSource" => $this->newsText->textSource,
+//                "link" => $this->newsText->link
+//            ]
+        ];
+
+        return $this->removeKeysWhereValueIsNull($result);
+    }
+}
