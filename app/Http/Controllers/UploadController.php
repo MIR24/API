2349533e04245ca57bed32c358de41b985dea4a9
IvanZ <apsent86@gmail.com>
2019-01-29 12:13:36 +0300

    add documentation swagger and error with empty file

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -18,7 +18,101 @@ use Illuminate\Support\Facades\Validator;
  */
 class UploadController extends Controller
 {
-
+    /**
+     *  @OA\Schema(
+     *   schema="apiRequestUploadForSimple",
+     *   type="object",
+     *   description="Форма запроса  только  для описание, без файла",
+     *   @OA\Property(property="title", type="string",
+     *     description="Обязательный. Заголовок/название"),
+     *   @OA\Property(property="desc", type="string",
+     *     description="Обязательный. Описание"),
+     *   @OA\Property(property="name", type="string",
+     *     description="Обязательный. Имя отправителя"),
+     *  @OA\Property(property="profile", type="string",
+     *     description="Обязательный. Профиль пользователя"),
+     *   @OA\Property(property="token", type="string",
+     *     description="Обязательный. Идентификатор, получаемый после удачной авторизации"),
+     *  @OA\Property(property="email", type="string",
+     *     description="Не обязательный. Электронная почта пользователя."),
+     *
+     * ),
+     *
+     *  @OA\Schema(
+     *   schema="apiRequestUploadForFile",
+     *   type="object",
+     *   description="Форма запроса, если загружается файл или видео",
+     *   @OA\Property(property="title", type="string",
+     *     description="Обязательный. Заголовок/название"),
+     *   @OA\Property(property="desc", type="string",
+     *     description="Обязательный. Описание"),
+     *   @OA\Property(property="name", type="string",
+     *     description="Обязательный. Имя отправителя"),
+     *  @OA\Property(property="profile", type="string",
+     *     description="Обязательный. Профиль пользователя"),
+     *   @OA\Property(property="token", type="string",
+     *     description="Обязательный. Идентификатор, получаемый после удачной авторизации"),
+     *  @OA\Property(property="email", type="string",
+     *     description="Не обязательный. Электронная почта пользователя."),
+     *  @OA\Property(property="file", type="file",
+     *     description="Изображение или видео файл в форматах ('image/png','image/jpeg','image/jpg','video/3gp','video/mpeg','video/mp4')"),
+     * ),
+     *
+     *  @OA\Schema(
+     *   schema="apiResponseUploadForSimple",
+     *   type="string",
+     *   description="Унифицированная форма ответа",
+     * ),
+     *
+     * @OA\Post(
+     *   path="/mobile/v1/upload",
+     *   tags={"Mobile Api"},
+     *   summary="Запрос для записи файдов",
+     *
+     *   @OA\RequestBody(
+     *       description="Унифицированная форма запроса",
+     *
+     *   @OA\MediaType(
+     *       mediaType="multipart/form-data",
+     *       @OA\Schema(
+     *          ref="#/components/schemas/apiRequestUploadForFile",
+     *       )
+     *     ),
+     *
+     *   @OA\MediaType(
+     *       mediaType="application/x-www-form-urlencoded",
+     *       @OA\Schema(
+     *          ref="#/components/schemas/apiRequestUploadForSimple"
+     *       )
+     *     ),
+     *
+     *   ),
+     *
+     *   @OA\Response(
+     *      response=200,
+     *      description="OK",
+     *      @OA\JsonContent(ref="#/components/schemas/apiResponseUploadForSimple", example="Your upload was submitted successfully.")
+     *   ),
+     *
+     *   @OA\Response(
+     *      response=403,
+     *      description="Forbidden. Проверте токен доступа",
+     *      @OA\JsonContent(ref="#/components/schemas/apiResponseUploadForSimple", example="Wrong token.")
+     *   ),
+     *
+     *   @OA\Response(
+     *      response=400,
+     *      description="Bad Request. Проверте поля обязательные для заполнения, ",
+     *      @OA\JsonContent(ref="#/components/schemas/apiResponseUploadForSimple", example="Title not specified.")
+     *   ),
+     *
+     *   @OA\Response(
+     *      response=500,
+     *      description="Server error. Внутренния ошибка сервера",
+     *      @OA\JsonContent(ref="#/components/schemas/apiResponseUploadForSimple", example="Server error")
+     *   ),
+     * )
+     */
     public function upload(Request $request)
     {
 
@@ -62,6 +156,10 @@ class UploadController extends Controller
         /**
          * @var $file UploadedFile
          */
+
+        if(!$request->files->count()){
+            throw new UploadException("multipart/form-data used, but no files found.",400);
+        }
 
         foreach ($request->file() as $file) {
 
