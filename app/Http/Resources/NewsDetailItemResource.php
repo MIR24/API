a588c4819e2ffa8b259c995abf5e51d558b95e50
Обловацкий Юрий <jury@observat.by>
2019-01-29 10:46:25 +0300

    Documentation for POST /api/mobile/v1/newsById

--- app/Http/Resources/NewsDetailItemResource.php
+++ app/Http/Resources/NewsDetailItemResource.php
@@ -6,13 +6,16 @@ namespace App\Http\Resources;
 use App\Http\AppJsonResource;
 
 
-class NewsItemResource extends AppJsonResource
+class NewsDetailItemResource extends AppJsonResource
 {
     use NewsItemTrait;
 
     public function toArray($request)
     {
         $result = $this->getCommonResult();
+
+        $result["textWithTags"] = $this->newsText['textWithTags'];
+        $result["textSource"] = $this->newsText['textSource'];
 
         return $this->removeKeysWhereValueIsNull($result);
     }
