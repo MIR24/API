79036fb69bd2c4a7753034ea10a72fd0e81cc5ea
IvanZ <apsent86@gmail.com>
2019-02-16 17:08:12 +0300

    add SmartTvImporte

--- app/Library/Services/Import/SmartTvImporter.php
+++ app/Library/Services/Import/SmartTvImporter.php
@@ -3,29 +3,51 @@
 namespace App\Library\Services\Import;
 
 
+use App\Broadcasts;
+use App\Channel;
 use Illuminate\Support\Facades\DB;
 
 class SmartTvImporter
 {
+
+    private $params;
+
+    const MIR24 = 'mir24';
+
+    const MIRHD = 'mirhd';
+
+    /**
+     * SmartTvImporter constructor.
+     * @param $params array
+     */
+    public function __construct($params)
+    {
+        $this->params = $params;
+    }
+
+
     public function getChannels(): array
     {
         # TODO статический массив возвращать и всё. Или вообще из БД убрать, только в конфиге хранить
-        $query = "select id, title as name from tags where type = 8";
+        $query = "select id, title as name from tags where type = ?";
 
-        return DB::connection('mir24')->select($query);
+        return DB::connection(self::MIR24)->select($query, $this->params['mir24_tags']);
     }
+
 
     public function saveChannels($channels): self
     {
-        $stream = "http://onair.mir24.tv";
-        $streamLogo = "http://onair.mir24.tv/images/custom/logo.png";
-        $query = "INSERT INTO channels (id, name, stream_shift, stream_live, logo) "
-            . "VALUES (?,?,?,?,?) "
-            . "ON DUPLICATE KEY "
-            . "UPDATE name = VALUES(name), stream_shift = VALUES(stream_shift), stream_live = VALUES(stream_live), logo = VALUES(logo)";
-
         foreach ($channels as $channel) {
-            DB::insert($query, [$channel->id, $channel->name, "", $stream, $streamLogo]);
+            if (isset($this->params['streams'][$channel->id])) {
+                Channel::updateOrCreate(
+                    ['name' => $channel->name], // Unique field is name...Oh, a lot of trouble will bring us a name change
+                    [
+                        'stream_shift' => $this->params['streams'][$channel->id]['stream'],
+                        'stream_live' => $this->params['streams'][$channel->id]['live'],
+                        'logo' => $this->params['streams'][$channel->id]['logo'],
+                    ]
+                );
+            }
         }
 
         return $this;
@@ -33,21 +55,70 @@ class SmartTvImporter
 
     public function getBroadcasts(): array
     {
-//        $query = "select t1.id, t1.title as name
-//            from tags t1
-//            left join tag_tag tt on tt.related_tag=t1.id
-//            left join tags t2 on t2.id = tt.tag_id
-//            where t1.type = 8 and t2.type=4";
-//            where t1.type = 4 and t2.type=8";
-//        tags.title где type = 4 и который связан с текущим каналом (тег типа 8) через tag_tag
 
-//        return DB::connection('mir24')->select($query);
-        return []; # TODO
+        $query = "SELECT teleprogramm_id, title, description, min_age, start FROM teleprogramm_mirhd " .
+            "where `start` > ?  order by `start` asc";
+
+        $start_time = (new \DateTime("now - {$this->params['tv_program_end_period']} minute"))->format(DATE_W3C);
+
+        return DB::connection(self::MIRHD)->select($query, [$start_time]);
     }
 
-    public function saveBroadcasts($broadcasts): self
+    public function saveBroadcasts($broadcasts, $category_id, $channel_id): self
     {
-        # TODO
+        $end = null;
+        $buff = null;
+
+        $this->addTimeEnd($broadcasts);
+
+        foreach ($broadcasts as $broadcast) {
+            /**
+             * @var $res Broadcasts
+             */
+            $atributes = [
+                'title' => $broadcast->title,
+                'subtitle' => $broadcast->description,
+                'age_restriction' => $broadcast->min_age,
+                'day_of_week' => (new \DateTime($broadcast->start))->format('l'),
+                'time_begin' => $broadcast->start,
+                'time_end' => isset($broadcast->end) ? $broadcast->end : $broadcast->start,
+                'category_id' => $category_id,
+                'channel_id' => $channel_id,
+            ];
+
+            $res = Broadcasts::find($broadcast->teleprogramm_id);
+
+            if ($res)
+                $res->update($atributes);
+            else
+                $res=new Broadcasts(array_merge(['id' => $broadcast->teleprogramm_id],$atributes));
+
+            $res->save();
+        }
         return $this;
     }
+
+    private function addTimeEnd(&$broadcasts)
+    {
+
+        $past = null;
+
+        foreach ($broadcasts as $broadcast) {
+
+            if ($past == null) {
+                $past = $broadcast;
+            } else {
+                $past->end = (new \DateTime($broadcast->start))->format("Y-m-d H:i:s");
+                $past = $broadcast;
+            }
+
+        }
+
+        reset($broadcasts);
+        //We can not single out the end of the last item in the program,
+        //because we do not know when to start the next
+        //TODO find duration or end tim last elemetn into tv program
+        array_pop($broadcasts);
+    }
+
 }
