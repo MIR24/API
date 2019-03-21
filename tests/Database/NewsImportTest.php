076128a97ea92ee45e812f8cf25d1d5b567f3035
Обловацкий Юрий <jury@observat.by>
2019-03-21 14:28:18 +0300

    tests/Database/NewsImportTest::testCategoryImporter

--- tests/Database/NewsImportTest.php
+++ tests/Database/NewsImportTest.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 
+use App\Library\Services\Import\Mir24CategoryImporter;
 use App\Library\Services\Import\Mir24CountryImporter;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -12,22 +13,44 @@ class NewsImportTest extends TestCase
 
     public function testCountryImporter()
     {
-        $countriesNane = ['БЕЛАРУСЬ', 'РОССИЯ', 'ЕВРОПА', 'АЗИЯ'];
+        $countriesName = ['БЕЛАРУСЬ', 'РОССИЯ', 'ЕВРОПА', 'АЗИЯ'];
         /** @var  $importer Mir24CountryImporter */
         $importer = \App::make(Mir24CountryImporter::class);
 
         $countries = $importer->getCountries();
         $this->assertTrue(count($countries) > 0);
-        foreach ($countriesNane as $countryName) {
+        foreach ($countriesName as $countryName) {
             $this->assertDatabaseMissing('country', [
                 'name' => $countryName,
             ]);
         }
 
         $importer->saveCountries($countries);
-        foreach ($countriesNane as $countryName) {
+        foreach ($countriesName as $countryName) {
             $this->assertDatabaseHas('country', [
                 'name' => $countryName,
+            ]);
+        }
+    }
+
+    public function testCategoryImporter()
+    {
+        $categoriesName = ["ЭКОНОМИКА", "СТИЛЬ ЖИЗНИ", "СПОРТ", "В МИРЕ"];
+        /** @var  $importer Mir24CategoryImporter */
+        $importer = \App::make(Mir24CategoryImporter::class);
+
+        $categories = $importer->getCategories();
+        $this->assertTrue(count($categories) > 0);
+        foreach ($categoriesName as $categoryName) {
+            $this->assertDatabaseMissing('categories', [
+                'name' => $categoryName,
+            ]);
+        }
+
+        $importer->updateCategories($categories);
+        foreach ($categoriesName as $categoryName) {
+            $this->assertDatabaseHas('categories', [
+                'name' => $categoryName,
             ]);
         }
     }
